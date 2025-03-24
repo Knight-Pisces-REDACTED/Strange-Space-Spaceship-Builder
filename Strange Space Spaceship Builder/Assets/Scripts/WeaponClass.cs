49ec21c75d5f7146e9d268c2ed329190abf84a20
Knight-Pisces-REDACTED <augieeturner@gmail.com>
2025-03-24 14:21:29 -0500

    Need to do more

--- Strange Space Spaceship Builder/Assets/Scripts/WeaponClass.cs
+++ Strange Space Spaceship Builder/Assets/Scripts/WeaponClass.cs
@@ -1,20 +1,26 @@
-using System;
-using UnityEngine;
-
 public class WeaponClass {
 
     public string name;
+    public Techs reqTech;
+    public WeaponSupertypes type;
     public double damage;
     public double range;
     public double accuracy;
     public double weight;
     public double cost;
-    public Techs reqTech;
     public double falloff;
 
 
-    public WeaponClass(string weaponName, double weaponDamage, double weaponRange, double baseAccuracy, double weight, double cost, Techs tech, ){
-
+    public WeaponClass(string weaponName, double weaponDamage, double weaponRange, double baseAccuracy, double baseWeight, double baseCost, Techs tech, double falloffRate, WeaponSupertypes baseType){
+        name = weaponName;
+        reqTech = tech;
+        type = baseType; 
+        weight = baseWeight;
+        cost = baseCost;
+        accuracy = baseAccuracy;
+        range = weaponRange;
+        damage = weaponDamage;
+        falloff = falloffRate;
     }
 
 }
---
+++ Strange Space Spaceship Builder/Assets/Scripts/WeaponSupertypes.cs
@@ -0,0 +1,5 @@
+public enum WeaponSupertypes{
+    Direct,
+    Electronic,
+    Guided,
+}
